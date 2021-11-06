ad40d7d590a33457359622e05c5fe0db5a3bc1d7
T <titrochas86@gmail.com>
2021-11-06 19:13:07 +0100

    test particules murales

--- src/level.py
+++ src/level.py
@@ -78,7 +78,7 @@ class Level:
 
         w1 = Weapon(cadence=1500, recoil=3, damage=100)
         w2 = Weapon(400, 0.5, 12)
-        Player(self.world, 10, 10, 1, self.world.player_group, weapon=w1)
+        Player(self.world, 10, 10, 1, self.world.player_group, weapon=w2)
 
         self.spawn_enemys()
 
