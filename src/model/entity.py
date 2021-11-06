22711debbbf9427dde26c43705a9b148a2d59862
T <titrochas86@gmail.com>
2021-11-06 10:21:23 +0100

    vitesse du joueur

--- src/model/entity.py
+++ src/model/entity.py
@@ -35,10 +35,10 @@ class Entity(DynamicObject):
         super(Entity, self).move(vector)
 
         # Limite la vélocité à 10
-        if vector.x > 10:
-            vector.x = 10
-        if vector.x < -10:
-            vector.x = -10
+        if vector.x > 8:
+            vector.x = 8
+        if vector.x < -8:
+            vector.x = -8
 
     def receive_damage(self, damage):
         """
